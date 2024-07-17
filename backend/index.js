58d67f0d2543340b5edbafe76a221675219c92c8
uphadeharsh45 <128834201+uphadeharsh45@users.noreply.github.com>
2024-07-17 19:20:12 +0530

    slider fixed and node backend ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const axios = require('axios');
 connectToMongo();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
